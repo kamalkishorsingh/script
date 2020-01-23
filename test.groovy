b44d56b9df58748ab7f4fc3c806a4f54629530bb
kamalkishorsingh <kamalkishorsingh3@gmail.com>
2020-01-23 12:35:33 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -15,18 +15,21 @@ def setVar() {
 }
 
 def setSha() {
-  dir(WORKING_DIR) {
-    GIT_SHA = sh(returnStdout: true, script: 'git rev-parse HEAD')
-  }
-}
+	setVar()
+	dir(WORKING_DIR) {
+		GIT_SHA = sh(returnStdout: true, script: 'git rev-parse HEAD')
+		}
+	}
 
 def clone() {
 	stage name: 'clone'
-    // github plugin, pipeline: github groovy libraries need to installed for this
-    dir(WORKING_DIR) {
+	// github plugin, pipeline: github groovy libraries need to installed for this
+	dir(WORKING_DIR) {
       checkout poll: false, scm: [$class: 'GitSCM', branches: [[name: GITHUB_PR_SOURCE_BRANCH]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'ChangelogToBranch', options: [compareRemote: "origin", compareTarget: 'master']], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: GIT_CREDS, url: "https://github.com/"+GIT_ORG+"/"+GIT_REPO+".git"]]]
-    }
-}
+	}
+}	
+
+    
 
 
 node("master") {
