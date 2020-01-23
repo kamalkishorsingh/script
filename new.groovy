73770770e09039835974101bf21fa52925798f51
kamalkishorsingh <kamalkishorsingh3@gmail.com>
2020-01-23 13:24:00 +0530

    Create new.groovy

---
+++ new.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+
+env.BRANCH_NAME = BRANCH_NAME
+env.BUILD_ENV = BUILD_ENV
+
+def build_artifact() {
+    git ([url: 'git@github.com:elarahq/'+GIT_REPO+'.git', branch: BRANCH_NAME, changelog: true, poll: true])
+  }
+  
