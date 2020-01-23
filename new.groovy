139e928cd667477e77092944e6dacbdcef556559
kamalkishorsingh <kamalkishorsingh3@gmail.com>
2020-01-23 14:02:44 +0530

    Update new.groovy

--- new.groovy
+++ new.groovy
@@ -1,10 +1,12 @@
 #!/usr/bin/env groovy
 
-env.BRANCH_NAME = BRANCH_NAME
+//env.BRANCH_NAME = BRANCH_NAME
 //env.BUILD_ENV = BUILD_ENV
 
 def build_artifact() {
    // git ([url: 'git@github.com:'+GIT_ORG+'/'+GIT_REPO+'.git', branch: BRANCH_NAME, changelog: true, poll: true])
-    git ([url: 'https://git@github.com:'+GIT_ORG+'/'+GIT_REPO+'.git', branch: BRANCH_NAME, changelog: true, poll: true])
+    //git ([url: 'https://git@github.com:'+GIT_ORG+'/'+GIT_REPO+'.git', branch: BRANCH_NAME, changelog: true, poll: true])
+  // git branch: 'kk', credentialsId: 'kamalkishorsingh', url: 'https://github.com/kamalkishorsingh/test-nginx.git'
+   git branch: [[name: BRANCH_NAME]], credentialsId: 'kamalkishorsingh', url: 'https://github.com/'+GIT_ORG+'/'+GIT_REPO+''
   }
   
