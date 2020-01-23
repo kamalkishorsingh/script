923deaac9749fd11e6cc01ac49ab31a9b0c2143e
kamalkishorsingh <kamalkishorsingh3@gmail.com>
2020-01-23 14:04:36 +0530

    Update new.groovy

--- new.groovy
+++ new.groovy
@@ -1,13 +1,13 @@
 #!/usr/bin/env groovy
 
-//env.BRANCH_NAME = BRANCH_NAME
+env.BRANCH_NAME = BRANCH_NAME
 //env.BUILD_ENV = BUILD_ENV
 
 def clone() {
    // git ([url: 'git@github.com:'+GIT_ORG+'/'+GIT_REPO+'.git', branch: BRANCH_NAME, changelog: true, poll: true])
-    //git ([url: 'https://git@github.com:'+GIT_ORG+'/'+GIT_REPO+'.git', branch: BRANCH_NAME, changelog: true, poll: true])
+    git ([url: 'https://git@github.com:'+GIT_ORG+'/'+GIT_REPO+'.git', branch: BRANCH_NAME, changelog: true, poll: true])
   // git branch: 'kk', credentialsId: 'kamalkishorsingh', url: 'https://github.com/kamalkishorsingh/test-nginx.git'
-   git branch: [[name: BRANCH_NAME]], credentialsId: 'kamalkishorsingh', url: 'https://github.com/'+GIT_ORG+'/'+GIT_REPO+''
+  // git branch: [[name: BRANCH_NAME]], credentialsId: 'kamalkishorsingh', url: 'https://github.com/'+GIT_ORG+'/'+GIT_REPO+''
   }
   
 node("master") {
