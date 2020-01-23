0eb5934cff9a166dd12d45d76846392b57ac0c03
kamalkishorsingh <kamalkishorsingh3@gmail.com>
2020-01-23 13:40:05 +0530

    Update new.groovy

--- new.groovy
+++ new.groovy
@@ -4,6 +4,7 @@ env.BRANCH_NAME = BRANCH_NAME
 //env.BUILD_ENV = BUILD_ENV
 
 def build_artifact() {
-    git ([url: 'git@github.com:elarahq/'+GIT_REPO+'.git', branch: BRANCH_NAME, changelog: true, poll: true])
+   // git ([url: 'git@github.com:'+GIT_ORG+'/'+GIT_REPO+'.git', branch: BRANCH_NAME, changelog: true, poll: true])
+    git ([url: 'https://git@github.com:'+GIT_ORG+'/'+GIT_REPO+'.git', branch: BRANCH_NAME, changelog: true, poll: true])
   }
   
