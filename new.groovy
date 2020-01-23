a6dd55aebbd29a04a40008ef15523854c5782898
kamalkishorsingh <kamalkishorsingh3@gmail.com>
2020-01-23 14:07:01 +0530

    Update new.groovy

--- new.groovy
+++ new.groovy
@@ -7,7 +7,7 @@ def clone() {
    // git ([url: 'git@github.com:'+GIT_ORG+'/'+GIT_REPO+'.git', branch: BRANCH_NAME, changelog: true, poll: true])
   //  git ([url: 'https://git@github.com:'+GIT_ORG+'/'+GIT_REPO+'.git', branch: BRANCH_NAME, changelog: true, poll: true])
   // git branch: 'kk', credentialsId: 'kamalkishorsingh', url: 'https://github.com/kamalkishorsingh/test-nginx.git'
-  git branch: 'kk', credentialsId: 'kamalkishorsingh', url: 'https://github.com/kamalkishorsingh/test-nginx.git'
+  git branch: 'master', credentialsId: 'kamalkishorsingh', url: 'https://github.com/kamalkishorsingh/test-nginx.git'
   }
   
 node("master") {
