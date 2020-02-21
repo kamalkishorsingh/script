722706f4a0a5199df0ac325efeab7752b9549861
kamalkishorsingh <kamalkishorsingh3@gmail.com>
2020-02-21 05:57:56 +0530

    Deleted extra data

--- new.groovy
+++ new.groovy
@@ -4,8 +4,6 @@ env.BRANCH_NAME = BRANCH_NAME
 //env.BUILD_ENV = BUILD_ENV
 
 def clone() {
-   // git ([url: 'git@github.com:'+GIT_ORG+'/'+GIT_REPO+'.git', branch: BRANCH_NAME, changelog: true, poll: true])
-  //  git ([url: 'https://git@github.com:'+GIT_ORG+'/'+GIT_REPO+'.git', branch: BRANCH_NAME, changelog: true, poll: true])
   // git branch: 'kk', credentialsId: 'kamalkishorsingh', url: 'https://github.com/kamalkishorsingh/test-nginx.git'
   //git branch: 'master', credentialsId: 'kamalkishorsingh', url: 'https://github.com/kamalkishorsingh/test-nginx.git'
 	git url: 'https://github.com/kamalkishorsingh/test-nginx.git'
