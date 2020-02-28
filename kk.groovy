262b4b8ecebc28bca96d53376f338fd6addd432d
kamalkishorsingh <kamalkishorsingh3@gmail.com>
2020-02-28 11:27:58 +0530

    Update kk.groovy

--- kk.groovy
+++ kk.groovy
@@ -72,9 +72,9 @@ def estatus(){
               script {
               mail (to: 'kamal271992@gmail.com',
                 subject: "Job '${env.JOB_NAME}' (${env.BUILD_NUMBER}) is waiting for input",
-                body: "Please go to ${env.BUILD_URL}. This Approval for FE");
-              env.RequestedAction = input message: 'Do you want to confirm deployment on FE ??', ok: 'Proceed for FE',
-              parameters: [choice(choices: "Approve\nReject", description: 'You want to Approve/Reject validation of FE.', name: 'Requested_Action')]
+                body: "Please go to ${env.BUILD_URL}. This Approval for Deployment");
+              env.RequestedAction = input message: 'Do you want to confirm deployment??', ok: 'Proceed for Deployment',
+              parameters: [choice(choices: "Approve\nReject", description: 'You want to Approve/Reject validation of Deployment.', name: 'Requested_Action')]
               
             }
 }
