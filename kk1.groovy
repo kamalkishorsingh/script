7b2e765a7fd4dcae540d6567af807a0c746874dd
kamalkishorsingh <kamalkishorsingh3@gmail.com>
2020-02-29 22:32:04 +0530

    Update kk1.groovy

--- kk1.groovy
+++ kk1.groovy
@@ -26,7 +26,7 @@ def compile() {
   if(language == "python" ) {
 //   println "Hello World!"
    sh label: '', script: 'echo "it is Python"'
-	  echo ${currentBuild.number}"
+   println "BUILD_NUMBER"
    sh "rm -rf ${WORKSPACE}/${service}.tar"
    // println "${WORKSPACE}"
    sh "cd ${WORKSPACE}/repo/ ; tar -czf ../${service}.tar ."
